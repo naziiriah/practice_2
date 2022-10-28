874e3316732967f2a519244e7389784f4f5ce398
naziiriah <naziistry7@gmail.com>
2022-10-28 13:12:43 +0100

    seeting up repo

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+
+void main() => runApp(MaterialApp(
+      initialRoute: "/home",
+      routes: {
+        '/home': (context) => Home(),
+        // '/location': (context) => ChangeLocation()
+      },
+    ));
